e05546d5d78f1f7113923c66c4c0041916e5c168
KeletsoMakofane <65560061+KeletsoMakofane@users.noreply.github.com>
2021-10-21 06:02:16 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fcca8c6bc116ef118f46.js"
+    "url": "webpack-runtime-921c6b23ee15c555b226.js"
   },
   {
     "url": "framework-6bcb7bb9b6581723c30e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bf030324215d7c0d076df5e003416349"
+    "revision": "ce599287823c372fe8ce4e29f7fdc51a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-27c66d5dcd0cba7d37a1.js"
